1bf08bb13ee200f5916c707a22e67558b8bf27a4
Dorus Leliveld <dorusleliveld@gmail.com>
2017-09-26 14:43:35 +0200

    standard version working for AWS

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-INSTALL_COMMAND="pip3 install"
+INSTALL_COMMAND="sudo pip-3.4 install"
 dependencies="warcio requests requests_file boto3 botocore py4j spark"
 
 for dep in $dependencies; do
