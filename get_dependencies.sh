f4cef365d359734adc0a8c22f36559d31a044fa2
Dorus Leliveld <dorusleliveld@gmail.com>
2017-10-09 10:27:45 +0200

    Installing pyspark on AWS breaks the script.
    Installing pyspark from this script via pip3 breaks on the EC2 machines.
    I originally left it in to have a single get_dependency script for both
    local development, as well as the AWS cluster. Removed pyspark for
    dependency list for now, as this seems the easiest fix.

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 INSTALL_COMMAND="sudo pip-3.4 install"
-dependencies="warcio requests requests_file boto3 botocore py4j pyspark"
+dependencies="warcio requests requests_file boto3 botocore py4j"
 
 for dep in $dependencies; do
     $INSTALL_COMMAND $dep
